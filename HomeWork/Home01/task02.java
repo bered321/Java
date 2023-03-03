db31f36c0fa0c7cb8e143a2723c00df136367e60
be_red <bered321@inbox.ru>
2023-03-03 13:50:15 +0300

    add solution task 2

---
+++ HomeWork/Home01/task02.java
@@ -0,0 +1,22 @@
+//Задача 2. Вывести все простые числа от 1 до 1000
+
+package HomeWork.Home01;
+
+public class task02 {
+   public static void main(String[] args) {
+    int flag;
+    for (int i = 2; i <= 1000; i++) {
+        flag = 1;
+        for (int j = 2; j <= i/2; j++) {
+            if (i % j == 0)
+            {
+                flag = 0;
+                break;                        
+            }
+        }
+        if (flag == 1) {
+            System.out.print(i+" ");
+        }
+    }
+   } 
+}
