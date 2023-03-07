43fd695ed68544cb3c29e1d12d544654b98bb1a0
be_red <bered321@inbox.ru>
2023-03-07 21:49:05 +0300

    add solution task 2

---
+++ HomeWork/Home02/task02.java
@@ -0,0 +1,57 @@
+// Дана строка (получение через обычный текстовый файл!!!)
+// "фамилия":"Иванов","оценка":"5","предмет":"Математика"
+// "фамилия":"Петрова","оценка":"4","предмет":"Информатика"
+// Написать метод(ы), который распарсит строку и, используя StringBuilder, создаст строки вида:
+// Студент [фамилия] получил [оценка] по предмету [предмет].
+// Пример вывода в консоль:
+// Студент Иванов получил 5 по предмету Математика.
+// Студент Петрова получил 4 по предмету Информатика.
+// Студент Краснов получил 5 по предмету Физика.
+
+package HomeWork.Home02;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class task02 {
+    public static String[] ReadLineFromFile(String pathway) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(pathway));
+        String str;
+        int size = 0;
+        while ((str = br.readLine()) != null) {
+            size += 1;
+        }
+        br.close();
+        String[] listData = new String[size];
+
+        int i = 0;
+        BufferedReader br1 = new BufferedReader(new FileReader(pathway));
+        while ((str = br1.readLine()) != null) {
+            listData[i] = str;
+            i += 1;
+        }
+        br1.close();
+        return listData;
+    }
+
+    public static StringBuilder PrintLine(String line) {
+        String line1 = line.replaceAll("\"", "");
+        StringBuilder result = new StringBuilder("");
+        String[] arrayData = line1.split(",");
+        String[] listName = { "Студент ", " получил ", " по предмету " };
+        for (int i = 0; i < arrayData.length; i++) {
+            String[] arrData = arrayData[i].split(":");
+            result.append(listName[i]);
+            result.append(arrData[1]);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        String[] arrayData = ReadLineFromFile("HomeWork/Home02/file.txt");
+        for (int i = 0; i < arrayData.length; i++) {
+            System.out.println(PrintLine(arrayData[i]));
+        }
+    }
+}
