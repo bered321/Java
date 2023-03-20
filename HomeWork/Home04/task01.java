2b1e871a848a740a7d1f8b0aa12d7da843e51e83
be_red <bered321@inbox.ru>
2023-03-20 19:32:08 +0300

    add solution task 1

---
+++ HomeWork/Home04/task01.java
@@ -0,0 +1,25 @@
+package HomeWork.Home04;
+
+import java.util.LinkedList;
+
+public class task01 {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+        System.out.println("Исходный список: " + list);
+        LinkedList<String> reversedList = reverseLinkedList(list);
+        System.out.println("Перевернутый список: " + reversedList);
+    }
+
+    public static LinkedList<String> reverseLinkedList(LinkedList<String> list) {
+        LinkedList<String> reversedList = new LinkedList<>();
+        for (int i = list.size() - 1; i >= 0; i--) {
+            String element = list.get(i);
+            reversedList.add(element);
+        }
+        return reversedList;
+    }
+}
